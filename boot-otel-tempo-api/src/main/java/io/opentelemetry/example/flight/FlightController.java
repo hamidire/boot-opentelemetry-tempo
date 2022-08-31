62019057e2beaa5b5838c5005757c28caa519f66
Hamid Shahsavari <shahsavari.h@tiddev.com>
2022-08-31 14:47:05 +0430

    adding promethous counter and ...

--- boot-otel-tempo-api/src/main/java/io/opentelemetry/example/flight/FlightController.java
+++ boot-otel-tempo-api/src/main/java/io/opentelemetry/example/flight/FlightController.java
@@ -1,7 +1,12 @@
 package io.opentelemetry.example.flight;
 
+import java.io.IOException;
 import java.util.List;
 
+import io.prometheus.client.Counter;
+import io.prometheus.client.Gauge;
+import io.prometheus.client.Histogram;
+import io.prometheus.client.Summary;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +15,24 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FlightController {
+
+	// Counter should have Exemplars when the OpenTelemetry agent is attached.
+	private final Counter requestCounter = Counter.build().name("requests_total").help("Total number of requests.")
+			.labelNames("path").register();
+
+	// Gauges don't have Exemplars.
+	private final Gauge lastRequestTimestamp = Gauge.build().name("last_request_timestamp")
+			.help("unix time of the last request").labelNames("path").register();
+
+	// Histogram should have Exemplars when the OpenTelemetry agent is attached.
+	private final Histogram requestDurationHistogram = Histogram.build().name("request_duration_histogram")
+			.help("Request duration in seconds").labelNames("path")
+			.buckets(0.001, 0.002, 0.003, 0.004, 0.005, 0.006, 0.007, 0.008, 0.009).register();
+
+	// Summaries don't have Exemplars
+	private final Summary requestDurationSummary = Summary.build().name("request_duration_summary")
+			.help("Request duration in seconds").labelNames("path").quantile(0.75, 0.01).quantile(0.85, 0.01)
+			.register();
 	
 	 private static final Logger LOGGER = LoggerFactory.getLogger(FlightController.class);
 	
@@ -20,9 +43,38 @@ public class FlightController {
 	}
 	
 	@GetMapping("/flights")
-    public List<Flight> greeting(@RequestParam(value = "origin", defaultValue = "India") String origin) {
+    public List<Flight> greeting(@RequestParam(value = "origin", defaultValue = "India") String origin) throws IOException {
     	LOGGER.info("Before Service Method Call");
-        return flightService.getFlights(origin);
+		String path = "/hello%hamish";
+		requestCounter.labels(path).inc();
+		lastRequestTimestamp.labels(path).setToCurrentTime();
+		Histogram.Timer histogramRequestTimer = requestDurationHistogram.labels(path).startTimer();
+		Summary.Timer summaryRequestTimer = requestDurationSummary.labels(path).startTimer();
+		List<Flight> list = flightService.getFlights(origin);
+		histogramRequestTimer.observeDuration();
+		summaryRequestTimer.observeDuration();
+		try {
+			// Generate some random errors
+			randomError(path);
+
+			// Randomly sleeps a bit
+			try {
+				Thread.sleep((long) (Math.random() * 1000));
+			} catch (InterruptedException e) {
+				throw new IOException(e);
+			}
+
+
+		} finally {
+			histogramRequestTimer.observeDuration();
+			summaryRequestTimer.observeDuration();
+		}
+        return list;
     }
+	private void randomError(String path) throws IOException {
+		if (Math.random() > 0.9) {
+			throw new IOException("Random error with " + path + "!");
+		}
+	}
 
 }
